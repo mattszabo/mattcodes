4b91f23a35cb8ca838c8b151f87008ad2c3ca3b6
Matt Szabo <szabo.matthew@gmail.com>
2017-07-24 21:18:17 +1000

    first

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import MattCodesApp from './components/MattCodesApp.jsx'
+
+ReactDOM.render(<MattCodesApp />, document.getElementById('root'))
