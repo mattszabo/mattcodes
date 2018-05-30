0bc4ce52373d4414e90f9195a1ad1e50284a1131
wekilledit <szabo.matthew@gmail.com>
2018-05-30 22:34:50 +1000

    react-router-dom for routing - implemented documentation example

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,14 +1,55 @@
 import React from 'react'
 import { Navbar, Content, Footer } from '../'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
 
 export class Layout extends React.Component {
   render() {
-    return (
+    const About = () => (
       <div>
-        <Navbar />
-        <Content />
-        <Footer />
+        <h2>About</h2>
       </div>
+    )
+
+    const Topic = ({ match }) => (
+      <div>
+        <h3>{match.params.topicId}</h3>
+      </div>
+    )
+
+    const Topics = ({ match }) => (
+      <div>
+        <h2>Topics</h2>
+        <ul>
+          <li>
+            <Link to={`${match.url}/rendering`}>Rendering with React</Link>
+          </li>
+          <li>
+            <Link to={`${match.url}/components`}>Components</Link>
+          </li>
+          <li>
+            <Link to={`${match.url}/props-v-state`}>Props v. State</Link>
+          </li>
+        </ul>
+
+        <Route path={`${match.path}/:topicId`} component={Topic} />
+        <Route
+          exact
+          path={match.path}
+          render={() => <h3>Please select a topic.</h3>}
+        />
+      </div>
+    )
+    return (
+      <Router>
+        <div>
+          <Navbar />
+          <hr />
+
+          <Route exact path="/" component={Content} />
+          <Route path="/about" component={About} />
+          <Route path="/topics" component={Topics} />
+        </div>
+      </Router>
     )
   }
 }
--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -1,7 +1,20 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export class Navbar extends React.Component {
   render() {
-    return <nav>yo</nav>
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/topics">Topics</Link>
+        </li>
+      </ul>
+    )
   }
 }
