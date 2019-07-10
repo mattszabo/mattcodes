caca662997ad7dbd1bdcd9ae02f6b05bf85e98fe
Matt Szabo <szabo.matthew@gmail.com>
2019-07-10 18:46:15 +1000

    update copy

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <div>
       <h1>React Hooks Clicker</h1>
-      <p>You clicked {count} times</p>
+      <p>You've clicked {count} times</p>
       <CounterButton handleClick={handleButtonClick} text="Click me" />
     </div>
   );
