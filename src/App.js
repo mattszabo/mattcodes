28cd2a73ed2063f7b3d1615deda082cfe3fdd1f3
Matt Szabo <szabo.matthew@gmail.com>
2019-07-10 18:33:59 +1000

    start cleaning up
    unregister serviceworker
    add normalise.css
    remove some CRA files

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React, { useState } from 'react';
 import CounterButton from './components/CounterButton';
 
-import './App.css';
-
 const App = () => {
-	const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0);
 
-	const handleButtonClick = () => {
-		setCount(count + 1);
-	};
+  const handleButtonClick = () => {
+    setCount(count + 1);
+  };
 
-	return (
-		<div>
-			<h1>React Hooks Clicker</h1>
-			<p>You clicked {count} times</p>
-			<CounterButton handleClick={handleButtonClick} text="Click me" />
-		</div>
-	);
+  return (
+    <div>
+      <h1>React Hooks Clicker</h1>
+      <p>You clicked {count} times</p>
+      <CounterButton handleClick={handleButtonClick} text="Click me" />
+    </div>
+  );
 };
 
 export default App;
